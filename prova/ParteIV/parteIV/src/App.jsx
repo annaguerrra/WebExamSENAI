716d34c50ec46e5a693c8ee1d4828c8a4d1c8ed6
annaguerrra <annaguerracastro@gmail.com>
2025-09-10 10:44:24 -0300

    cor

--- prova/ParteIV/parteIV/src/App.jsx
+++ prova/ParteIV/parteIV/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -10,9 +8,10 @@ function App() {
     <>
       <div>
       </div>
-      <h1>4.a Contador</h1>
-      <div className="card">
-        <h1>{count}</h1>
+        <h1>4.a Contador</h1>
+        <div className="card">
+        <h1 className='count'>{count}</h1>
+   
         <button onClick={() => setCount((count) => count + 1)}>
           +
         </button>
